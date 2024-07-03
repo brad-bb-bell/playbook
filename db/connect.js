4e5aac9c60164d3e8bee0a487fd35fb60c8a2fa7
Brad Bell <brad.bb.bell@gmail.com>
2024-07-03 10:24:57 -0600

    fixes errors

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  return mongoose.connect(url)
 }
 
 module.exports = connectDB
