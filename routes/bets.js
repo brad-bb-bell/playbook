c4bf70e99839f63b8c76d520f30f60ff63743fcb
Brad Bell <brad.bb.bell@gmail.com>
2024-07-03 11:49:01 -0600

    adds routes

--- routes/bets.js
+++ routes/bets.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllBets } = require('../controllers/bets')
+const {
+  getAllBets,
+  createBet,
+  getBet,
+  updateBet,
+  deleteBet,
+} = require('../controllers/bets')
 
-router.route('/').get(getAllBets)
+router.route('/').get(getAllBets).post(createBet)
+router.route('/:id').get(getBet).patch(updateBet).delete(deleteBet)
 
 module.exports = router
