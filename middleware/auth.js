a02426dec98798de33e6bf73d8c544a473f33c84
Brad Bell <brad.bb.bell@gmail.com>
2024-08-07 10:26:25 -0600

    consolodates custom errors

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,25 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
 const authenticationMiddleware = async (req, res, next) => {
-  console.log(req.header.authorization)
-  next()
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401)
+    //401 is authentication error
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const { id, username } = decoded
+    req.user = { id, username }
+    next()
+  } catch (error) {
+    //token might be expired
+    throw new CustomAPIError('Not authorized to access this route', 401)
+  }
 }
 
-module.export = authenticationMiddleware
+module.exports = authenticationMiddleware
