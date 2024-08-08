3d1319eea523e9f95c668ea94c3467125a32193d
Brad Bell <brad.bb.bell@gmail.com>
2024-08-08 15:49:13 -0600

    updates register functionality in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = jwt.sign(
+    { userID: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    },
+  )
+  res.status(StatusCodes.CREATED).json({ username: user.username, token })
 }
 
 const login = async (req, res) => {
