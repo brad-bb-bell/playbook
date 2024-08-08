6404705ccd388de10a548680616289a5f52b9cfd
Brad Bell <brad.bb.bell@gmail.com>
2024-08-08 16:13:22 -0600

    adds register functionality in auth controller

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -26,5 +27,13 @@ UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userID: this._id, username: this.username },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME },
+  )
+}
 
 module.exports = mongoose.model('User', UserSchema)
