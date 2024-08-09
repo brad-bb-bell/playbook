bf83412635238a2e0b141564cfbb2ae63e5ce6e0
Brad Bell <brad.bb.bell@gmail.com>
2024-08-09 15:45:26 -0600

    completes updateBet

--- controllers/bets.js
+++ controllers/bets.js
@@ -27,19 +27,22 @@ const getBet = async (req, res) => {
 }
 
 const updateBet = async (req, res) => {
-  try {
-    const { id: betID } = req.params
-    if (!mongoose.Types.ObjectId.isValid(betID)) {
-      return res.status(400).json({ msg: `Invalid ID format: ${betID}` })
-    }
-    const bet = await Bet.findOneAndUpdate({ _id: betID }, req.body, {
-      new: true,
-      runValidators: true,
-    })
-    res.status(200).json({ msg: 'Bet updated', data: bet })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+  //could validate the body here.. no empty fields, etc
+  const {
+    user: { userID },
+    params: { id: betID },
+  } = req
+
+  const bet = await Bet.findOneAndUpdate(
+    { _id: betID, createdBy: userID },
+    req.body,
+    { new: true, runValidators: true },
+  )
+
+  if (!bet) {
+    throw new NotFoundError(`No bet with ID: ${betID}`)
   }
+  res.status(StatusCodes.OK).json({ bet })
 }
 
 const deleteBet = async (req, res) => {
