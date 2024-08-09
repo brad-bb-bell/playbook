be414ae4c1729514932bd435adb0801145098c8f
Brad Bell <brad.bb.bell@gmail.com>
2024-08-09 10:20:05 -0600

    removes mainRouter again and adds auth middleware to all bets routes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ require('express-async-errors')
 
 const express = require('express')
 const app = express()
-const mainRouter = require('./routes/main')
 const betsRouter = require('./routes/bets')
 const authRouter = require('./routes/auth')
 
 const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/auth')
 
 //error handlers
 const notFoundMiddleware = require('./middleware/not-found')
@@ -17,8 +17,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(express.json())
 
 //routes
-app.use('/api/v1', mainRouter)
-app.use('/api/v1/bets', betsRouter)
+app.use('/api/v1/bets', authenticateUser, betsRouter)
 app.use('/api/v1/auth', authRouter)
 
 const port = process.env.PORT || 3000
